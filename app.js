0f202bd70cc66a4e91534ccc8ef5c22b46ed4468
zaeemulhassan12 <zaeemulhassan819.com>
2026-04-13 21:05:00 +0500

    FIRST CLASS

---
+++ app.js
@@ -0,0 +1,36 @@
+//module wrap 
+
+// console.log(__filename);
+// console.log(__dirname);
+// const log = require('./logger');
+// // console.log(logger)
+
+// log('hy its me zaeem ');
+
+//module Path 
+
+// const path = require('path');
+
+// var pathobj = path.parse(__filename);
+
+
+// console.log(pathobj);
+
+//os module load 
+
+var osi = require('os');
+
+var TotalMemory  = osi.totalmem();
+
+var FreeMemory = osi.freemem();
+
+
+console.log('TotalMemory'+TotalMemory);
+console.log('freeMemory'+FreeMemory);
+
+//template string 
+//ES6 /ES2015 :ECMAScript 
+
+
+console.log(`ZAEEM UL HASSAN PC Total memory :${TotalMemory}`);
+console.log(`ZAEEM UL HASSAN PC Free Memory :${FreeMemory}`);
